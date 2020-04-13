583c59ecda52309be6d5fccc3a1d79afee33c1f1
Rishi Khanna <rishi.khanna@asurion.com>
2020-04-13 10:43:10 -0500

    Raising the version

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Rishi Khannaç'
+        name: 'Rishi Khanna'
     })
 })
 
